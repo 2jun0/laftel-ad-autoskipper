2667e9682b2a1b2b89546c6b4af42e7934b1cfc2
2jun0 <soo28819@naver.com>
2022-09-30 18:50:29 +0900

    Update: 디버그 로그 수정

--- chrome/js/utils/log.js
+++ chrome/js/utils/log.js
@@ -1,5 +1,5 @@
-const DEBUG_MODE = true;
+const DEBUG_MODE = chrome.runtime.id == 'dcpckjplmdcgdbapaepejenmkknkoimg';
 
 export const printLog = (...args) => {
-  if (DEBUG_MODE) console.log(...args);
+  if (DEBUG_MODE) console.log('[laftel-ad-autoskipper]', ...args);
 };
