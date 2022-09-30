493e754790386c08e6fc843b4a6654c1940bb903
2jun0 <soo28819@naver.com>
2022-09-30 21:07:23 +0900

    Fix: log 에서 디버그 모드가 반대로 설정되는 현상 수정

--- chrome/js/utils/log.js
+++ chrome/js/utils/log.js
@@ -1,4 +1,4 @@
-const DEBUG_MODE = chrome.runtime.id == 'dcpckjplmdcgdbapaepejenmkknkoimg';
+const DEBUG_MODE = chrome.runtime.id != 'dcpckjplmdcgdbapaepejenmkknkoimg';
 
 export const printLog = (...args) => {
   if (DEBUG_MODE) console.log('[laftel-ad-autoskipper]', ...args);
